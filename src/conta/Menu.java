c742aefb2c76f5ef961e4f8e820af9adf5c2df4d
livehass <silva.felipe12@hotmail.com>
2023-12-13 16:39:46 -0300

    add new cores.java cores.util

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -2,6 +2,8 @@ package conta;
 
 import java.util.Scanner;
 
+import conta.util.Cores;
+
 public class Menu {
 
 	public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class Menu {
 		int op;
 
 		while (true) {
-			System.out.println("***********************************************************\n"
+			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
+					+"***********************************************************\n"
 					+ "*			                                  *\n"
 					+ "*-------------------BANCO-DA-GENERETION-------------------*\n"
 					+ "*			                                  *\n"
